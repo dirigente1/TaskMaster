28580a69e55308e043192fe5f478ef793e93f5bf
dirigente1 <dirigente.fusaglia@nucleoispettoriforestali.eu>
2026-04-10 18:37:23 +0200

    Create file.sh

---
+++ file.sh
@@ -0,0 +1,15 @@
+# Vai nella cartella del progetto
+cd tuo-repository
+
+# Elimina i file indesiderati
+git rm src/types
+git rm src/utils
+git rm src/components
+git rm src/ActivityForm.tsx
+git rm src/storage.ts
+git rm src/index.ts
+git rm README.md
+
+# Commit e push
+git commit -m "Rimossi file indesiderati"
+git push
